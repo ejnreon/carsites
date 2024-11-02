2ac734c2f3786c27130a3ba2eaabc3e53530e6ad
daniel.lyko <daniel.lyko@gmail.com>
2024-11-02 19:20:27 +0100

    End of section 2

---
+++ src/AuctionService/RequestHelpers/MappingProfiles.cs
@@ -0,0 +1,21 @@
+using System;
+using AuctionDTOs;
+using AuctionService.DTOs;
+using AuctionService.Entities;
+using AutoMapper;
+
+namespace AuctionService.RequestHelpers;
+
+public class MappingProfiles:Profile
+{
+
+public MappingProfiles() {
+     CreateMap<Auction, AuctionDTO>().IncludeMembers(x => x.Item);
+     CreateMap<Item, AuctionDTO>();
+     CreateMap<CreateAuctionDTO, Auction>()
+        .ForMember(d => d.Item, o => o.MapFrom(s => s));
+        //mapowanie d = destination , o = mapowany obiekt , na s = source (CreateAuctionDTO)// czyli dla (d)estination Auction.Item, zmapuj (o)biekt.MapFrom (s) ource  
+    CreateMap<CreateAuctionDTO,Item>();
+}
+
+}
